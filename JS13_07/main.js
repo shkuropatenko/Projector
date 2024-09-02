d107ee9b6d03869293a8aea5c1830867116ea1e6
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2024-09-02 15:57:24 -0400

    Added HW #7

--- JS13_07/main.js
+++ JS13_07/main.js
@@ -5,79 +5,79 @@
 // 1
 // BOOM!
 
-// function detonatorTimer(delay) {
-//   let timerId = setInterval(() => {
-//     if (delay > 0) {
-//       console.log(delay);
-//       delay--;
-//     } else {
-//       console.log("BOOM!");
-//       clearInterval(timerId);
-//     }
-//   }, 1000);
-// }
+function detonatorTimer(delay) {
+  let timerId = setInterval(() => {
+    if (delay > 0) {
+      console.log(delay);
+      delay--;
+    } else {
+      console.log("BOOM!");
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
 
-// detonatorTimer(3);
+detonatorTimer(3);
 
-// function detonatorTimer2(delay) {
-//   setTimeout(() => {
-//     if (delay > 0) {
-//       console.log(delay);
-//       delay--;
-//       detonatorTimer2(delay);
-//     } else {
-//       console.log("BOOM!");
-//     }
-//   }, 1000);
-// }
-// detonatorTimer2(3);
+function detonatorTimer2(delay) {
+  setTimeout(() => {
+    if (delay > 0) {
+      console.log(delay);
+      delay--;
+      detonatorTimer2(delay);
+    } else {
+      console.log("BOOM!");
+    }
+  }, 1000);
+}
+detonatorTimer2(3);
 
-// let me = {
-//   name: "Mykola",
-//   residency: "Lviv",
-//   gender: "male",
-//   age: 31,
-//   hobby: "crafting",
-//   defaultMood: "focused",
-//   currentMood: "sleepy",
-//   height: "35cm",
-//   weight: "8kg",
-//   introduce() {
-//     console.log(`My name is ${this.name} and I live in ${this.residency}`);
-//   },
-//   prognose() {
-//     console.log(`I hope that next year I'm gonna be ${this.age + 1}`);
-//   },
-//   describeMyMood() {
-//     console.log(
-//       `Mostly I'm ${this.defaultMood}, but now I'm ${this.currentMood}`
-//     );
-//   },
-//   myGender() {
-//     console.log(`My gender is ${this.gender}`);
-//   },
-//   myHobby() {
-//     console.log(`I like the ${this.hobby} beer`);
-//   },
-//   petParameters() {
-//     console.log(`My pet height is ${this.height} and weight is ${this.weight}`);
-//   },
-// };
+let me = {
+  name: "Mykola",
+  residency: "Lviv",
+  gender: "male",
+  age: 31,
+  hobby: "crafting",
+  defaultMood: "focused",
+  currentMood: "sleepy",
+  height: "35cm",
+  weight: "8kg",
+  introduce() {
+    console.log(`My name is ${this.name} and I live in ${this.residency}`);
+  },
+  prognose() {
+    console.log(`I hope that next year I'm gonna be ${this.age + 1}`);
+  },
+  describeMyMood() {
+    console.log(
+      `Mostly I'm ${this.defaultMood}, but now I'm ${this.currentMood}`
+    );
+  },
+  myGender() {
+    console.log(`My gender is ${this.gender}`);
+  },
+  myHobby() {
+    console.log(`I like the ${this.hobby} beer`);
+  },
+  petParameters() {
+    console.log(`My pet height is ${this.height} and weight is ${this.weight}`);
+  },
+};
 
-// me.introduce();
-// me.prognose();
-// me.describeMyMood();
-// me.myGender();
-// me.myHobby();
-// me.petParameters();
+me.introduce();
+me.prognose();
+me.describeMyMood();
+me.myGender();
+me.myHobby();
+me.petParameters();
 
-// let securedSelfIntroduce = me.introduce.bind(me);
-// let securedSelfPrognose = me.prognose.bind(me);
-// let securedSelfDescribeMyMood = me.petParameters.bind(me);
+let securedSelfIntroduce = me.introduce.bind(me);
+let securedSelfPrognose = me.prognose.bind(me);
+let securedSelfDescribeMyMood = me.petParameters.bind(me);
 
-// setTimeout(securedSelfIntroduce, 1000); // виведе коректний результат
-// setTimeout(securedSelfPrognose, 2000); // виведе коректний результат
-// setTimeout(securedSelfDescribeMyMood, 3000); // виведе коректний результат
+setTimeout(securedSelfIntroduce, 1000); // виведе коректний результат
+setTimeout(securedSelfPrognose, 2000); // виведе коректний результат
+setTimeout(securedSelfDescribeMyMood, 3000); // виведе коректний результат
 
 // тут напишіть довільну функцію яка робить якусь роботу зі своїми аргументами та виводить результат в консоль
 function someFunction(text) {
