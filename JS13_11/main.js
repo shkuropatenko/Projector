4ce0e41c394fd0451da80c2f897810362d3a064a
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2024-09-30 21:09:51 -0400

    Added private variable

--- JS13_11/main.js
+++ JS13_11/main.js
@@ -1,20 +1,27 @@
+// Базовий клас для всіх транспортних засобів
 class Vehicle {
+  #engine;
+
   constructor(name, maker, engine) {
-    this.name = name;
-    this.maker = maker;
-    this._engine = engine; // приватна властивість - двигун
+    this.name = name; // назва транспорту
+    this.maker = maker; // виробник
+    this.#engine = engine; // приватна властивість
   }
 
   getType() {
-    return `Двигун цього транспортного засобу: ${this._engine}.`;
+    return `Двигун цього транспортного засобу: ${this.#engine}.`;
   }
 
   getDetails() {
     return `Назва: ${this.name}, Виробник: ${this.maker}.`;
   }
 
-  getEngineDetails() {
-    return `Технічні характеристики двигуна: ${this._engine}.`;
+  #getEngineDetails() {
+    return `Технічні характеристики двигуна: ${this.#engine}.`;
+  }
+
+  showEngineDetails() {
+    return this.#getEngineDetails();
   }
 }
 
@@ -22,7 +29,7 @@ class Vehicle {
 class LandVehicle extends Vehicle {
   constructor(name, maker, engine, wheels) {
     super(name, maker, engine);
-    this.wheels = wheels;
+    this.wheels = wheels; // кількість коліс
   }
 
   drive() {
@@ -39,7 +46,7 @@ class LandVehicle extends Vehicle {
 class WaterVehicle extends Vehicle {
   constructor(name, maker, engine, type) {
     super(name, maker, engine);
-    this.type = type;
+    this.type = type; // тип водного транспорту (катер, яхта і т.д.)
   }
 
   sail() {
@@ -56,7 +63,7 @@ class WaterVehicle extends Vehicle {
 class Car extends LandVehicle {
   constructor(name, maker, engine, wheels, fuel) {
     super(name, maker, engine, wheels);
-    this.fuel = fuel;
+    this.fuel = fuel; // тип палива (бензин, дизель, електро)
   }
 
   refuel() {
@@ -68,7 +75,7 @@ class Car extends LandVehicle {
 class Boat extends WaterVehicle {
   constructor(name, maker, engine, type, length) {
     super(name, maker, engine, type);
-    this.length = length;
+    this.length = length; // довжина корабля
   }
 
   anchor() {
@@ -76,11 +83,11 @@ class Boat extends WaterVehicle {
   }
 }
 
-// Унікальний клас для літаків
+// Унікальний клас для літаків (повітряний транспорт)
 class Airplane extends Vehicle {
   constructor(name, maker, engine, wingSpan) {
     super(name, maker, engine);
-    this.wingSpan = wingSpan;
+    this.wingSpan = wingSpan; // розмах крил
   }
 
   fly() {
@@ -96,7 +103,7 @@ class Airplane extends Vehicle {
 class Bicycle extends LandVehicle {
   constructor(name, maker, engine, wheels, gearCount) {
     super(name, maker, engine, wheels);
-    this.gearCount = gearCount;
+    this.gearCount = gearCount; // кількість передач
   }
 
   pedal() {
@@ -104,11 +111,13 @@ class Bicycle extends LandVehicle {
   }
 }
 
+// Приклади створення об'єктів
 const car1 = new Car("Tesla Model S", "Tesla", "електро", 4, "електрика");
 const boat1 = new Boat("Sea Breeze", "Yamaha", "двигун 300HP", "яхта", 20);
 const airplane1 = new Airplane("Boeing 747", "Boeing", "турбореактивний", 68);
 const bicycle1 = new Bicycle("Giant", "Giant", "немає двигуна", 2, 21);
 
+// Виклик методів
 console.log(car1.getType());
 console.log(car1.getDetails());
 console.log(car1.refuel());
@@ -124,4 +133,6 @@ console.log(airplane1.landing());
 
 console.log(bicycle1.getDetails());
 console.log(bicycle1.pedal());
-console.log(bicycle1.service()); // спільний метод для наземних транспортів
+console.log(bicycle1.service());
+
+console.log(car1.showEngineDetails());
